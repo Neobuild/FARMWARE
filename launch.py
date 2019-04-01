b54fd484544bef16d1516014459352d0313922b3
Neobuild <stagiaire@neobuild.lu>
2019-04-01 16:12:15 +0200

    Update launch.py

--- launch.py
+++ launch.py
@@ -2,16 +2,19 @@ import os
 from FARMWARE import MyFarmware
 from FARMWARE import Sequence
 from farmware_tools import log
-from structure import Structure
 import CeleryPy as cp
+from structure import Structure
 from farmware_tools import send_celery_script
 
 FARMWARE_NAME = "jhempbot"
-
+"""
+def get_env(key, type_=str):
+    return type_(os.environ["{}_{}".format(FARMWARE_NAME, key)])
+"""
 def main():
-    log("jhempbot")
-    farmware = MyFarmware(FARMWARE_NAME) 
-    farmware.run() 
+    log("jhempbot --> hello")
+    farmware = MyFarmware(FARMWARE_NAME)
+    farmware.run()
     
 if __name__ == "__main__":
     main()
